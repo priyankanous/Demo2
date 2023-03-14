fbe419364712963a8fa592ce716a8a6a033932ae
Andra Kavitha <kavithaa@nousinfo.com>
2023-03-14 18:14:35 +0530

    Upload New File

---
+++ rrr-backend/src/main/java/com/nous/rollingrevenue/common/rest/ErrorResponse.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package com.nous.rollingrevenue.common.rest;
+
+import java.util.List;
+
+/**
+ * @author Nous Infosystems
+ *
+ */
+public class ErrorResponse {
+
+	private String message;
+
+	private List<String> details;
+
+	public ErrorResponse(String message, List<String> details) {
+		super();
+		this.message = message;
+		this.details = details;
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * @return the details
+	 */
+	public List<String> getDetails() {
+		return details;
+	}
+
+	/**
+	 * @param details the details to set
+	 */
+	public void setDetails(List<String> details) {
+		this.details = details;
+	}
+
+	@Override
+	public String toString() {
+		return "ErrorResponse [message =" + message + "   details=" + details.toString() + "]";
+	}
+
+}
