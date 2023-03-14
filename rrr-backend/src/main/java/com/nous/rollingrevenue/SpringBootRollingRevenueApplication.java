5dd2c9c63cfbb3b1145a31ee87d0bc73da063776
Andra Kavitha <kavithaa@nousinfo.com>
2023-03-14 18:20:16 +0530

    Upload New File

---
+++ rrr-backend/src/main/java/com/nous/rollingrevenue/SpringBootRollingRevenueApplication.java
@@ -0,0 +1,13 @@
+package com.nous.rollingrevenue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRollingRevenueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRollingRevenueApplication.class, args);
+	}
+
+}
