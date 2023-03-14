20a10cb55d47a20dfd2b5d9e47a9e52411a661f7
Andra Kavitha <kavithaa@nousinfo.com>
2023-03-14 18:13:29 +0530

    Upload New File

---
+++ rrr-backend/src/main/java/com/nous/rollingrevenue/common/constant/Constants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.nous.rollingrevenue.common.constant;
+
+/**
+ * @author Nous Infosystems
+ *
+ */
+public class Constants {
+
+	public static final String REG_SUCC_EMAIL_SUB = "You registered sucessfully";
+
+}
