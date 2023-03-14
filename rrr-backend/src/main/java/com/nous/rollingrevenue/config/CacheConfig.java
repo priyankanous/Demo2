159b02c054c2f5ea993e077a077d287d543da97e
Andra Kavitha <kavithaa@nousinfo.com>
2023-03-14 18:16:15 +0530

    Upload New File

---
+++ rrr-backend/src/main/java/com/nous/rollingrevenue/config/CacheConfig.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.nous.rollingrevenue.config;
+
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Nous Infosystems
+ *
+ */
+@Configuration
+@EnableCaching
+public class CacheConfig {
+
+}
