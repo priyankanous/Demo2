662734266101086a126787832bea2ef78de2870d
Andra Kavitha <kavithaa@nousinfo.com>
2023-03-14 18:14:50 +0530

    Upload New File

---
+++ rrr-backend/src/main/java/com/nous/rollingrevenue/common/rest/RestMessage.java
@@ -0,0 +1,12 @@
+package com.nous.rollingrevenue.common.rest;
+
+/**
+ *
+ * @author Nous Infosystems
+ */
+public class RestMessage {
+
+	public static final String SUCCESS = "Sucess";
+
+	public static final String FAIL = "Fail";
+}
